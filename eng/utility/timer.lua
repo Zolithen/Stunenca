bbfbd1fcbc7956cc2ef3d4a642fe57c3b2119cfb
128_NyKi <nkindie79@gmail.com>
2020-03-28 20:48:22 +0100

    Update timer.lua

--- eng/utility/timer.lua
+++ eng/utility/timer.lua
@@ -4,19 +4,30 @@ function tmt:update(dt)
 	if self.time > 0 then
 		self.time = self.time - dt
 	else
-		if not self.every then self:callback() end
+		if not self.every then 
+			self:callback() 
+			if self.rev then
+				self.time = self.max_time
+			else
+				self = nil
+			end
+		else
+			self = nil
+		end
 	end
 	if self.every then
 		self:callback()
 	end
 end
 
-function new_timer(time, callback, every)
+function new_timer(time, callback, every, rev)
 	local t = {}
 
 	t.time = time;
-	t.callback = callback
-	t.every = every or false
+	t.max_time = time;
+	t.callback = callback;
+	t.every = every or false;
+	t.rev = rev or false;
 
 	setmetatable(t, {__index=tmt});
 
