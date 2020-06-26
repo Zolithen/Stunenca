3e5f7c9fa57169d46d125ca6fcde981e9efbbc6f
SuperHostile <superhostilegd@gmail.com>
2020-06-26 21:34:17 +0200

    added basic stencil helper

--- eng/gui/Stencil.lua
+++ eng/gui/Stencil.lua
@@ -1,13 +1,24 @@
+StencilNode = Node:extends("StencilNode");
 
-
-function reset_stencil()
-
+function StencilNode:init(sc, n)
+	StencilNode.super.init(self, sc, n, 0, 0);
+	self.boxes = {};
 end
 
-function add_box()
-
+function StencilNode:reset()
+	self.boxes = {};
 end
 
-function apply_stencil()
+function StencilNode:add(x, y, w ,h)
+	table.insert(self.boxes, {x, y, w, h});
+	return #self.boxes
+end
 
+function StencilNode:apply()
+	for i, v in ipairs(self.boxes) do
+		love.graphics.stencil(function()
+    		love.graphics.rectangle("fill", unpack(v));
+    	end, "replace", 1)
+	end
+	love.graphics.setStencilTest("greater", 0);
 end
